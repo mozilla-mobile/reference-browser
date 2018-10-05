5e307fcfc568520eecd38f39d460d4597228458b
Sebastian Kaspari <s.kaspari@gmail.com>
2018-10-05 11:15:57 +0200

    Remove unused dependencies.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,17 +14,8 @@ private object Versions {
 
     const val support_libraries = "27.1.1"
     const val constraint_layout = "1.1.2"
-    const val workmanager = "1.0.0-alpha09"
 
-    const val dokka = "0.9.16"
     const val android_gradle_plugin = "3.1.4"
-    const val bintray_gradle_plugin = "1.7.3"
-    const val maven_gradle_plugin = "2.1"
-    const val lint = "26.1.3"
-
-    const val jna = "4.5.2"
-
-    const val mozilla_app_services = "0.5.1"
 
     const val mozilla_android_components = "0.25.2"
 }
@@ -33,7 +24,6 @@ private object Versions {
 object Deps {
     const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val kotlin_coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
-    const val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
 
     const val mozilla_concept_engine = "org.mozilla.components:concept-engine:${Versions.mozilla_android_components}"
     const val mozilla_concept_tabstray = "org.mozilla.components:concept-tabstray:${Versions.mozilla_android_components}"
@@ -69,21 +59,6 @@ object Deps {
     const val support_constraintlayout = "com.android.support.constraint:constraint-layout:${Versions.constraint_layout}"
     const val support_compat = "com.android.support:support-compat:${Versions.support_libraries}"
 
-    const val arch_workmanager = "android.arch.work:work-runtime:${Versions.workmanager}"
-
-    const val tools_dokka = "org.jetbrains.dokka:dokka-android-gradle-plugin:${Versions.dokka}"
     const val tools_androidgradle = "com.android.tools.build:gradle:${Versions.android_gradle_plugin}"
     const val tools_kotlingradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    const val tools_bintray = "com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.bintray_gradle_plugin}"
-    const val tools_mavengradle = "com.github.dcendents:android-maven-gradle-plugin:${Versions.maven_gradle_plugin}"
-
-    const val tools_lint = "com.android.tools.lint:lint:${Versions.lint}"
-    const val tools_lintapi = "com.android.tools.lint:lint-api:${Versions.lint}"
-    const val tools_linttests = "com.android.tools.lint:lint-tests:${Versions.lint}"
-
-    const val mozilla_fxa = "org.mozilla.fxa_client:fxa_client:${Versions.mozilla_app_services}"
-    const val mozilla_sync_logins = "org.mozilla.sync15:logins:${Versions.mozilla_app_services}"
-
-    const val jna = "net.java.dev.jna:jna:${Versions.jna}@aar"
-    const val jnaForTest = "net.java.dev.jna:jna:${Versions.jna}@jar"
 }
