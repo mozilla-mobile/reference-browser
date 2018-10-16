d227f847bf52518ff9fdb7dd4e524c9f8d6149e6
Christian Sadilek <christian.sadilek@gmail.com>
2018-10-16 15:58:15 -0400

    Closes #10: Integrate service/firefox-accounts

--- app/src/main/java/org/mozilla/reference/browser/settings/SettingsActivity.kt
+++ app/src/main/java/org/mozilla/reference/browser/settings/SettingsActivity.kt
@@ -2,13 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.reference.browser
+package org.mozilla.reference.browser.settings
 
 import android.R.id.content
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), SettingsFragment.ActionBarUpdater {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +20,17 @@ class SettingsActivity : AppCompatActivity() {
                 commit()
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    override fun updateTitle(titleResId: Int) {
+        setTitle(titleResId)
     }
 }
--- app/src/main/java/org/mozilla/reference/browser/settings/SettingsFragment.kt
+++ app/src/main/java/org/mozilla/reference/browser/settings/SettingsFragment.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.reference.browser
+package org.mozilla.reference.browser.settings
 
 import android.content.Intent
 import android.os.Bundle
@@ -11,11 +11,18 @@ import android.support.v7.preference.Preference.OnPreferenceClickListener
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import org.mozilla.reference.browser.R
+import org.mozilla.reference.browser.R.string.pref_key_firefox_account
 import org.mozilla.reference.browser.ext.getPreferenceKey
 import org.mozilla.reference.browser.R.string.pref_key_sign_in
 import org.mozilla.reference.browser.R.string.pref_key_make_default_browser
+import org.mozilla.reference.browser.ext.requireComponents
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    interface ActionBarUpdater {
+        fun updateTitle(titleResId: Int)
+    }
 
     private val defaultClickListener = OnPreferenceClickListener { preference ->
         Toast.makeText(context, "${preference.title} Clicked", LENGTH_SHORT).show()
@@ -24,13 +31,33 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        setupPreferences()
+        getActionBarUpdater().apply {
+            updateTitle(R.string.settings)
+        }
+    }
+
+    private fun setupPreferences() {
         val signInKey = context?.getPreferenceKey(pref_key_sign_in)
+        val firefoxAccountKey = context?.getPreferenceKey(pref_key_firefox_account)
         val makeDefaultBrowserKey = context?.getPreferenceKey(pref_key_make_default_browser)
         val preferenceSignIn = findPreference(signInKey)
+        val preferenceFirefoxAccount = findPreference(firefoxAccountKey)
         val preferenceMakeDefaultBrowser = findPreference(makeDefaultBrowserKey)
+        val fxaIntegration = requireComponents.firefoxAccountsIntegration
 
-        preferenceSignIn.onPreferenceClickListener = defaultClickListener
+        preferenceSignIn.onPreferenceClickListener = getClickListenerForSignIn()
+        preferenceSignIn.isVisible = fxaIntegration.profile == null
+
+        preferenceFirefoxAccount.onPreferenceClickListener = getClickListenerForFirefoxAccount()
+        preferenceFirefoxAccount.isVisible = fxaIntegration.profile != null
+        preferenceFirefoxAccount.summary = fxaIntegration?.profile?.email
+
         preferenceMakeDefaultBrowser.onPreferenceClickListener = getClickListenerForMakeDefaultBrowser()
     }
 
@@ -47,4 +74,27 @@ class SettingsFragment : PreferenceFragmentCompat() {
             defaultClickListener
         }
     }
+
+    private fun getClickListenerForSignIn(): OnPreferenceClickListener {
+        return OnPreferenceClickListener { _ ->
+            activity?.finish()
+            requireComponents.firefoxAccountsIntegration.authenticate()
+            true
+        }
+    }
+
+    private fun getClickListenerForFirefoxAccount(): OnPreferenceClickListener {
+        return OnPreferenceClickListener { _ ->
+            fragmentManager?.beginTransaction()
+                    ?.replace(android.R.id.content, AccountSettingsFragment())
+                    ?.addToBackStack(null)
+                    ?.commit()
+            getActionBarUpdater().apply {
+                updateTitle(R.string.account_settings)
+            }
+            true
+        }
+    }
+
+    private fun getActionBarUpdater() = activity as ActionBarUpdater
 }
