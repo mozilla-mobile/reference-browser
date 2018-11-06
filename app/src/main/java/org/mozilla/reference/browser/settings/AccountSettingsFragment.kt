95ec7e4f0687e7f39c6c3acbdcde68888df491a3
Jonathan Almeida <git@jonalmeida.com>
2018-11-05 21:29:13 -0500

    Closes #38: Add Sync Now button
    Added the Sync Now button with a helper extension to show the time since
    last sync when the implementation lands.
    
    Also moved the sign out button down (and made it red) so that the two buttons weren't so
    close together.
    
    Closes mozilla-mobile/android-components#1223

--- app/src/main/java/org/mozilla/reference/browser/settings/AccountSettingsFragment.kt
+++ app/src/main/java/org/mozilla/reference/browser/settings/AccountSettingsFragment.kt
@@ -11,6 +11,7 @@ import org.mozilla.reference.browser.R
 import org.mozilla.reference.browser.ext.getPreferenceKey
 import org.mozilla.reference.browser.ext.requireComponents
 import org.mozilla.reference.browser.R.string.pref_key_sign_out
+import org.mozilla.reference.browser.R.string.pref_key_sync_now
 
 class AccountSettingsFragment : PreferenceFragmentCompat() {
 
@@ -18,14 +19,30 @@ class AccountSettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.account_preferences, rootKey)
 
         val signOutKey = context?.getPreferenceKey(pref_key_sign_out)
+        val syncNowKey = context?.getPreferenceKey(pref_key_sync_now)
+
+        // Sign Out
         val preferenceSignOut = findPreference(signOutKey)
         preferenceSignOut.onPreferenceClickListener = getClickListenerForSignOut()
+
+        // Sync Now
+        val preferenceSyncNow = findPreference(syncNowKey)
+        preferenceSyncNow.isEnabled = false // Make this `fxaIntegration.profile == null` when implemented
+        preferenceSyncNow.summary = getString(R.string.preferences_sync_never_synced_summary) // Use Long.timeSince()
+        preferenceSyncNow.onPreferenceClickListener = getClickListenerForSyncNow()
     }
 
     private fun getClickListenerForSignOut(): OnPreferenceClickListener {
-        return OnPreferenceClickListener { _ ->
+        return OnPreferenceClickListener {
             requireComponents.firefoxAccountsIntegration.logout()
             activity?.onBackPressed()
+            true
+        }
+    }
+
+    private fun getClickListenerForSyncNow(): OnPreferenceClickListener {
+        return OnPreferenceClickListener {
+            // Implement this Sync Now functionality when available.
             true
         }
     }
