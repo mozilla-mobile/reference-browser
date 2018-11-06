a1132da9013f08ac59894f70debc20e664c55820
Jonathan Almeida <git@jonalmeida.com>
2018-11-05 21:29:13 -0500

    Closes #38: Add Sync Now button
    Added the Sync Now button with a helper extension to show the time since
    last sync when the implementation lands.
    
    Also moved the sign out button down (and made it red) so that the two buttons weren't so
    close together.
    
    Closes mozilla-mobile/android-components#1223

---
+++ app/src/main/java/org/mozilla/reference/browser/ext/Long.kt
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.reference.browser.ext
+
+import android.content.Context
+import android.text.format.DateUtils
+import org.mozilla.reference.browser.R
+import java.util.Date
+import java.util.Calendar
+import java.util.GregorianCalendar
+
+@Suppress("MagicNumber")
+fun Long.timeSince(context: Context, time: Long): String {
+    val earliestValidSyncDate: Date = GregorianCalendar.getInstance().run {
+        set(2000, Calendar.JANUARY, 1, 0, 0, 0)
+        getTime()
+    }
+
+    if (Date(time).before(earliestValidSyncDate)) {
+        return context.getString(R.string.preferences_sync_never_synced_summary)
+    }
+
+    val relativeTimeSpanString = DateUtils.getRelativeTimeSpanString(time, this, DateUtils.MINUTE_IN_MILLIS)
+    return context.resources.getString(R.string.preferences_sync_last_synced_summary, relativeTimeSpanString)
+}
---
+++ app/src/main/java/org/mozilla/reference/browser/settings/CustomColorPreference.kt
@@ -0,0 +1,53 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.reference.browser.settings
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v7.preference.Preference
+import android.support.v7.preference.PreferenceViewHolder
+import android.util.AttributeSet
+import android.widget.TextView
+import org.mozilla.reference.browser.R
+
+/**
+ * This preference is used to define custom  colors for both title and summary texts.
+ * Color code #777777 (placeholder_grey) is used as the fallback color for both title and summary.
+ */
+class CustomColorPreference : Preference {
+    private var titleColor: Int = 0
+    private var summaryColor: Int = 0
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs,
+        defStyle) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet) {
+        context.obtainStyledAttributes(attrs, R.styleable.CustomColorPreference).apply {
+            titleColor = getColor(R.styleable.CustomColorPreference_titleColor,
+                ContextCompat.getColor(context, R.color.placeholder_grey))
+            summaryColor = getColor(R.styleable.CustomColorPreference_summaryColor,
+                ContextCompat.getColor(context, R.color.placeholder_grey))
+            recycle()
+        }
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder?) {
+        super.onBindViewHolder(holder)
+        holder?.let {
+            val title = it.itemView.findViewById(android.R.id.title) as TextView
+            val summary = it.itemView.findViewById(android.R.id.summary) as TextView
+            title.setTextColor(titleColor)
+            summary.setTextColor(summaryColor)
+        }
+    }
+}
