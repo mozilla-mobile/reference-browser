9b0ecfd498677dfd4a149348e03adc288bab98f0
Jonathan Almeida <jalmeida@mozilla.com>
2018-11-22 21:58:35 -0500

    Closes #213: Add menu option to close all tabs

--- app/src/main/java/org/mozilla/reference/browser/tabs/PrivatePage.kt
+++ app/src/main/java/org/mozilla/reference/browser/tabs/PrivatePage.kt
@@ -15,21 +15,21 @@ object PrivatePage {
      * Load and generate a private browsing page for the given url and html/css resources
      */
     fun createPrivateBrowsingPage(
-            context: Context,
-            url: String,
-            @RawRes htmlRes: Int = R.raw.private_mode,
-            @RawRes cssRes: Int = R.raw.private_style
+        context: Context,
+        url: String,
+        @RawRes htmlRes: Int = R.raw.private_mode,
+        @RawRes cssRes: Int = R.raw.private_style
     ): String {
         val css = context.resources.openRawResource(cssRes).bufferedReader().use {
             it.readText()
         }
 
         return context.resources.openRawResource(htmlRes)
-                .bufferedReader()
-                .use { it.readText() }
-                .replace("%pageTitle%", context.getString(R.string.private_browsing_title))
-                .replace("%pageBody%", context.getString(R.string.private_browsing_body))
-                .replace("%privateBrowsingSupportUrl%", url)
-                .replace("%css%", css)
+            .bufferedReader()
+            .use { it.readText() }
+            .replace("%pageTitle%", context.getString(R.string.private_browsing_title))
+            .replace("%pageBody%", context.getString(R.string.private_browsing_body))
+            .replace("%privateBrowsingSupportUrl%", url)
+            .replace("%css%", css)
     }
 }
