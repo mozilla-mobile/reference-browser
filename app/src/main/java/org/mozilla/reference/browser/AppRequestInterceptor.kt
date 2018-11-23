e2e9172a822fc2f3a4d169ef5475d68657fb18ed
Jonathan Almeida <jalmeida@mozilla.com>
2018-11-22 21:40:37 -0500

    Closes #215: Add private tab start page
    Adds a new redirect in the RequestInterceptor to "about:privatebrowsing"
    and also some HTML/CSS.

--- app/src/main/java/org/mozilla/reference/browser/AppRequestInterceptor.kt
+++ app/src/main/java/org/mozilla/reference/browser/AppRequestInterceptor.kt
@@ -10,10 +10,18 @@ import mozilla.components.browser.errorpages.ErrorType
 import mozilla.components.concept.engine.EngineSession
 import mozilla.components.concept.engine.request.RequestInterceptor
 import org.mozilla.reference.browser.ext.components
+import org.mozilla.reference.browser.tabs.PrivatePage
 
 class AppRequestInterceptor(private val context: Context) : RequestInterceptor {
     override fun onLoadRequest(session: EngineSession, uri: String): RequestInterceptor.InterceptionResponse? {
-        return context.components.firefoxAccountsIntegration.interceptor.onLoadRequest(session, uri)
+        return when (uri) {
+            "about:privatebrowsing" -> {
+                val page = PrivatePage.createPrivateBrowsingPage(context, uri)
+                return RequestInterceptor.InterceptionResponse.Content(page, encoding = "base64")
+            }
+
+            else -> context.components.firefoxAccountsIntegration.interceptor.onLoadRequest(session, uri)
+        }
     }
 
     override fun onErrorRequest(
---
+++ app/src/main/java/org/mozilla/reference/browser/tabs/PrivatePage.kt
@@ -0,0 +1,35 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.mozilla.reference.browser.tabs
+
+import android.content.Context
+import androidx.annotation.RawRes
+import org.mozilla.reference.browser.R
+
+object PrivatePage {
+    /**
+     * Load and generate a private browsing page for the given url and html/css resources
+     */
+    fun createPrivateBrowsingPage(
+            context: Context,
+            url: String,
+            @RawRes htmlRes: Int = R.raw.private_mode,
+            @RawRes cssRes: Int = R.raw.private_style
+    ): String {
+        val css = context.resources.openRawResource(cssRes).bufferedReader().use {
+            it.readText()
+        }
+
+        return context.resources.openRawResource(htmlRes)
+                .bufferedReader()
+                .use { it.readText() }
+                .replace("%pageTitle%", context.getString(R.string.private_browsing_title))
+                .replace("%pageBody%", context.getString(R.string.private_browsing_body))
+                .replace("%privateBrowsingSupportUrl%", url)
+                .replace("%css%", css)
+    }
+}
