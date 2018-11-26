1fdb878d2e3a3e6ca601bf558c062087ccc7c6d8
Jonathan Almeida <jalmeida@mozilla.com>
2018-11-26 14:41:08 -0500

    Refactor code to new TabsPanel

---
+++ app/src/main/java/org/mozilla/reference/browser/tabs/TabsPanel.kt
@@ -0,0 +1,121 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.mozilla.reference.browser.tabs
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.PorterDuff.Mode.SRC_IN
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import mozilla.components.feature.tabs.tabstray.TabsFeature
+import mozilla.components.ui.colors.R.color.photonPurple50
+import org.mozilla.reference.browser.R
+import org.mozilla.reference.browser.ext.components
+import org.mozilla.reference.browser.view.ToggleImageButton
+
+class TabsPanel @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : androidx.appcompat.widget.Toolbar(context, attrs) {
+    private lateinit var button: ToggleImageButton
+    private lateinit var privateButton: ToggleImageButton
+    private var tabsFeature: TabsFeature? = null
+    private var isPrivateTray = false
+    private var closeTabsTray: (() -> Unit)? = null
+
+    init {
+        setNavigationIcon(R.drawable.mozac_ic_back)
+        setNavigationOnClickListener {
+            closeTabsTray?.invoke()
+        }
+        inflateMenu(R.menu.tabstray_menu)
+        setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.newTab -> {
+                    with(components.tabsUseCases) {
+                        when (isPrivateTray) {
+                            true -> addPrivateTab.invoke("about:privatebrowsing", selectTab = true)
+                            false -> addTab.invoke("about:blank", selectTab = true)
+                        }
+                    }
+                    closeTabsTray?.invoke()
+                }
+                R.id.closeTab -> {
+                    with(components) {
+                        when (isPrivateTray) {
+                            true -> removePrivateSessions.invoke()
+                            false -> removeSessions.invoke()
+                        }
+                    }
+                }
+            }
+            true
+        }
+
+        button = ToggleImageButton(context).apply {
+            setImageDrawable(resources.getThemedDrawable(R.drawable.mozac_ic_tab))
+            setOnCheckedChangeListener { _, checked ->
+                if (checked) {
+                    updateToggleStates(this, privateButton, false)
+                }
+            }
+        }
+        privateButton = ToggleImageButton(context).apply {
+            setImageDrawable(resources.getThemedDrawable(R.drawable.mozac_ic_globe))
+            setOnCheckedChangeListener { _, checked ->
+                if (checked) {
+                    updateToggleStates(this, button, true)
+                }
+            }
+        }
+
+        addView(button)
+        addView(privateButton)
+    }
+
+    private fun updateToggleStates(ours: ToggleImageButton, theirs: ToggleImageButton, isPrivate: Boolean) {
+        // Tint our button
+        ours.drawable.colorTint(photonPurple50)
+
+        // Uncheck their button and remove tint
+        theirs.isChecked = false
+        theirs.drawable.colorFilter = null
+
+        // Store the state for the menu option
+        isPrivateTray = isPrivate
+
+        // Update the tabs tray with our filter
+        tabsFeature?.filterTabs { it.private == isPrivate }
+
+        // Update the menu option text
+        menu.findItem(R.id.closeTab).title = if (isPrivate) {
+            context.getString(R.string.menu_action_close_tabs_private)
+        } else {
+            context.getString(R.string.menu_action_close_tabs)
+        }
+    }
+
+    fun initialize(tabsFeature: TabsFeature?, closeTabsTray: () -> Unit) {
+        this.tabsFeature = tabsFeature
+        this.closeTabsTray = closeTabsTray
+
+        // initial opening of tabs tray should show regular tabs.
+        button.isChecked = true
+    }
+
+    private fun Resources.getThemedDrawable(@DrawableRes resId: Int) = getDrawable(resId, context.theme)
+
+    private fun Drawable.colorTint(@ColorRes color: Int) = apply {
+        mutate()
+        setColorFilter(ContextCompat.getColor(context, color), SRC_IN)
+    }
+
+    private val components = context.components
+}
---
+++ app/src/main/java/org/mozilla/reference/browser/view/ToggleImageButton.kt
@@ -0,0 +1,89 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.mozilla.reference.browser.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import android.view.accessibility.AccessibilityEvent
+import android.widget.Checkable
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import mozilla.components.support.base.android.Padding
+import mozilla.components.support.ktx.android.view.setPadding
+import org.mozilla.reference.browser.R
+
+/**
+ * Based off the internal version of com.google.android.material.internal.CheckableImageButton
+ */
+class ToggleImageButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = androidx.appcompat.R.attr.imageButtonStyle
+) : AppCompatImageButton(context, attrs, defStyle), Checkable {
+    private var onCheckedChangeListener: ((ToggleImageButton, Boolean) -> Unit)? = null
+
+    init {
+        ViewCompat.setAccessibilityDelegate(this, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityEvent(host: View, event: AccessibilityEvent) {
+                super.onInitializeAccessibilityEvent(host, event)
+                event.isChecked = this@ToggleImageButton.isChecked
+            }
+
+            override fun onInitializeAccessibilityNodeInfo(host: View, info: AccessibilityNodeInfoCompat) {
+                super.onInitializeAccessibilityNodeInfo(host, info)
+                info.isCheckable = true
+                info.isChecked = this@ToggleImageButton.isChecked
+            }
+        })
+
+        TypedValue().apply {
+            context.theme.resolveAttribute(R.attr.selectableItemBackgroundBorderless, this, true)
+            setBackgroundResource(resourceId)
+        }
+        setPadding(PADDING)
+
+        setChecked(attrs)
+    }
+
+    private fun setChecked(attrs: AttributeSet?) {
+        context.obtainStyledAttributes(attrs, R.styleable.ToggleImageButton).apply {
+            isChecked = getBoolean(R.styleable.ToggleImageButton_android_checked, false)
+            recycle()
+        }
+    }
+
+    override fun isChecked(): Boolean {
+        return isSelected
+    }
+
+    override fun setChecked(checked: Boolean) {
+        isSelected = checked
+        onCheckedChangeListener?.invoke(this, checked)
+    }
+
+    override fun toggle() {
+        isChecked = !isChecked
+    }
+
+    override fun performClick(): Boolean {
+        toggle()
+        return super.performClick()
+    }
+
+    fun setOnCheckedChangeListener(listener: (ToggleImageButton, Boolean) -> Unit) {
+        this.onCheckedChangeListener = listener
+    }
+
+    companion object {
+        private const val ACTION_PADDING_DP = 16
+        private val PADDING = Padding(ACTION_PADDING_DP, ACTION_PADDING_DP, ACTION_PADDING_DP, ACTION_PADDING_DP)
+    }
+}
