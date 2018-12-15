c46b8bc6a8a89f263e4e47399b01fcda1f5e3764
Grisha Kruglov <gkruglov@mozilla.com>
2018-12-14 20:03:11 -0800

    Follow-up to #324: undo the now-unneccessary check

--- app/src/main/java/org/mozilla/reference/browser/BrowserApplication.kt
+++ app/src/main/java/org/mozilla/reference/browser/BrowserApplication.kt
@@ -8,8 +8,6 @@ import android.app.Application
 import mozilla.components.support.base.log.Log
 import mozilla.components.support.base.log.sink.AndroidLogSink
 import org.mozilla.reference.browser.ext.isCrashReportActive
-import android.app.ActivityManager
-import android.content.Context
 
 class BrowserApplication : Application() {
     val components by lazy { Components(this) }
@@ -17,27 +15,12 @@ class BrowserApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // We only want to run this for the application process and not for sub
-        // processes e.g. the Gecko :tab process. Otherwise we'd be initializing
-        // our components multiple times and we'd also create the GeckoEngine
-        // and GeckoRuntime in the wrong process.
-        if (isChildProcess()) {
-            return
-        }
-
         // We want the log messages of all builds to go to Android logcat
         Log.addSink(AndroidLogSink())
 
         if (isCrashReportActive) {
             components.crashReporter.install(this)
         }
-    }
-
-    private fun isChildProcess(): Boolean {
-        val pid = android.os.Process.myPid()
-        val activityManager = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val processInfo = activityManager.runningAppProcesses.firstOrNull { it.pid == pid }
-        return processInfo?.processName?.contains(":") ?: false
     }
 
     companion object {
