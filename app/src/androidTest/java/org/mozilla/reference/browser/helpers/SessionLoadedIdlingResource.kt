a386fec65c9c5781335ca5ccc3e08f8aa3ad327d
Richard Pappalardo <rpappalax@gmail.com>
2018-12-21 14:13:50 -0800

    feat: add initial UI test framework (WIP)

---
+++ app/src/androidTest/java/org/mozilla/reference/browser/helpers/SessionLoadedIdlingResource.kt
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.reference.browser.helpers
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.IdlingResource
+import org.mozilla.reference.browser.BrowserApplication
+
+/**
+ * An IdlingResource implementation that waits until the current session is not loading anymore.
+ * Only after loading has completed further actions will be performed.
+ */
+class SessionLoadedIdlingResource : IdlingResource {
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun getName(): String {
+        return SessionLoadedIdlingResource::class.java.simpleName
+    }
+
+    override fun isIdleNow(): Boolean {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+                as BrowserApplication
+
+        val sessionManager = context.components.core.sessionManager
+
+        val session = sessionManager.selectedSession
+
+        return if (session?.loading == true) {
+            false
+        } else {
+            invokeCallback()
+            true
+        }
+    }
+
+    private fun invokeCallback() {
+        if (resourceCallback != null) {
+            resourceCallback!!.onTransitionToIdle()
+        }
+    }
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback) {
+        this.resourceCallback = callback
+    }
+}
