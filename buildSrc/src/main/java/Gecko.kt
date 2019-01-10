df307ec1422a9758cfdc2d037567d7928601107c
Sebastian Kaspari <s.kaspari@gmail.com>
2019-01-10 15:44:45 +0100

    Issue #367: Introduce aarch64 flavor and avoid APK split.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -9,4 +9,5 @@ private object GeckoVersions {
 object Gecko {
     const val geckoview_nightly_arm = "org.mozilla.geckoview:geckoview-nightly-armeabi-v7a:${GeckoVersions.nightly_version}"
     const val geckoview_nightly_x86 = "org.mozilla.geckoview:geckoview-nightly-x86:${GeckoVersions.nightly_version}"
+    const val geckoview_nightly_aarch64 = "org.mozilla.geckoview:geckoview-nightly-arm64-v8a:${GeckoVersions.nightly_version}"
 }
