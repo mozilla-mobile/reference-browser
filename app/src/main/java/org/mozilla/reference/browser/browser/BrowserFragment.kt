0605569ec2fc5a0d778b1b47c820c51827ced8c6
Jonathan Almeida <jalmeida@mozilla.com>
2019-01-20 17:24:40 -0500

    Optional: Picture-in-picture video support

--- app/src/main/java/org/mozilla/reference/browser/browser/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/reference/browser/browser/BrowserFragment.kt
@@ -25,18 +25,21 @@ import mozilla.components.support.ktx.android.content.isPermissionGranted
 import mozilla.components.support.ktx.android.view.enterToImmersiveMode
 import mozilla.components.support.ktx.android.view.exitImmersiveModeIfNeeded
 import org.mozilla.reference.browser.BackHandler
+import org.mozilla.reference.browser.UserInteractionHandler
 import org.mozilla.reference.browser.R
 import org.mozilla.reference.browser.ext.requireComponents
+import org.mozilla.reference.browser.pip.PictureInPictureFeature
 import org.mozilla.reference.browser.tabs.TabsTrayFragment
 
 @Suppress("TooManyFunctions")
-class BrowserFragment : Fragment(), BackHandler {
+class BrowserFragment : Fragment(), BackHandler, UserInteractionHandler {
     private lateinit var sessionFeature: SessionFeature
     private lateinit var tabsToolbarFeature: TabsToolbarFeature
     private lateinit var downloadsFeature: DownloadsFeature
     private lateinit var awesomeBarFeature: AwesomeBarFeature
     private lateinit var promptsFeature: PromptFeature
     private lateinit var fullScreenFeature: FullScreenFeature
+    private lateinit var pipFeature: PictureInPictureFeature
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_browser, container, false)
@@ -110,6 +113,8 @@ class BrowserFragment : Fragment(), BackHandler {
             sessionId, ::fullScreenChanged
         )
 
+        pipFeature = PictureInPictureFeature(requireComponents.core.sessionManager, requireActivity(), this)
+
         lifecycle.addObservers(
             sessionFeature,
             downloadsFeature,
@@ -151,6 +156,17 @@ class BrowserFragment : Fragment(), BackHandler {
         }
 
         return false
+    }
+
+    override fun onHomePressed(): Boolean {
+        if (pipFeature.onHomePressed()) {
+            return true
+        }
+        return false
+    }
+
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean) {
+        pipFeature.onPictureInPictureModeChanged(isInPictureInPictureMode)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
