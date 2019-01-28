4c2d4a2453b1331b6b34c170b565dd584407f7f6
Sebastian Kaspari <s.kaspari@gmail.com>
2019-01-28 17:45:05 +0100

    Update GeckoView to 66.0.20190128092811 (following A-C).

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 private object GeckoVersions {
-    const val nightly_version = "66.0.20190124104034"
+    const val nightly_version = "66.0.20190128092811"
 }
 
 object Gecko {
