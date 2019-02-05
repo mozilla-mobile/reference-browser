185a51e27dc83097a4a9212c344363f46050c51a
Jonathan Almeida <jalmeida@mozilla.com>
2019-02-05 11:04:07 -0500

    Update to A-C 0.42 and GeckoView
    Closes #565 as well.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 private object GeckoVersions {
-    const val nightly_version = "67.0.20190130001444"
+    const val nightly_version = "67.0.20190204092937"
 }
 
 object Gecko {
