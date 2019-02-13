d93f75ac292d91b99b9405f65cb7944633c4d9a6
Sebastian Kaspari <s.kaspari@gmail.com>
2019-02-13 14:42:58 +0100

    Update to Mozilla Android Components 0.43.0-SNAPSHOT.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -17,7 +17,7 @@ private object Versions {
     const val android_gradle_plugin = "3.2.1"
     const val appservices_gradle_plugin = "0.3.1"
 
-    const val mozilla_android_components = "0.42.0-SNAPSHOT"
+    const val mozilla_android_components = "0.43.0-SNAPSHOT"
 
     const val thirdparty_sentry = "1.7.10"
 
