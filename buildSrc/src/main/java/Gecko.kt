b4c3db3f0ec8e5fb11e64ef613eaad10d66d7f81
Mitchell Hentges <mhentges@mozilla.com>
2019-02-21 15:15:38 -0800

    Moves gecko nightly version to .buildconfig yaml

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -2,12 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-private object GeckoVersions {
-    const val nightly_version = "67.0.20190214044118"
-}
-
-object Gecko {
-    const val geckoview_nightly_arm = "org.mozilla.geckoview:geckoview-nightly-armeabi-v7a:${GeckoVersions.nightly_version}"
-    const val geckoview_nightly_x86 = "org.mozilla.geckoview:geckoview-nightly-x86:${GeckoVersions.nightly_version}"
-    const val geckoview_nightly_aarch64 = "org.mozilla.geckoview:geckoview-nightly-arm64-v8a:${GeckoVersions.nightly_version}"
+class Gecko(geckoNightlyVersion: String) {
+    val geckoview_nightly_arm = "org.mozilla.geckoview:geckoview-nightly-armeabi-v7a:$geckoNightlyVersion"
+    val geckoview_nightly_x86 = "org.mozilla.geckoview:geckoview-nightly-x86:$geckoNightlyVersion"
+    val geckoview_nightly_aarch64 = "org.mozilla.geckoview:geckoview-nightly-arm64-v8a:$geckoNightlyVersion"
 }
