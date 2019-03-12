97470caa32c436cede7871c0ab3332e54866bbe6
Arturo Mejia <arturomejiamarmol@gmail.com>
2019-03-12 10:36:40 -0400

    Upgrade AC dependency to 0.47.0-SNAPSHOT and GV to 67.0.20190312095443

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 private object GeckoVersions {
-    const val nightly_version = "67.0.20190307094951"
+    const val nightly_version = "67.0.20190312095443"
 }
 
 object Gecko {
