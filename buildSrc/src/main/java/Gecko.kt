6bf5ecbccda0dbb1a653721ad1d882bf24c49b7f
Christian Sadilek <christian.sadilek@gmail.com>
2019-05-01 14:09:14 -0400

    Issue #152: Integrate feature-readerview
    Closes #152, closes #153, closes #154, closes #155, closes #156, closes #202.

--- buildSrc/src/main/java/Gecko.kt
+++ buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object GeckoVersions {
-    const val nightly_version = "68.0.20190422094240"
+    const val nightly_version = "68.0.20190429095544"
 }
 
 object Gecko {
