d7aa8f55296b29cd9b755114ed79138e6e5e39c7
Mitchell Hentges <mhentges@mozilla.com>
2019-05-03 12:39:22 +0200

    [will-squash] Fix nimbledroid task

--- automation/taskcluster/upload_apk_nimbledroid.py
+++ automation/taskcluster/upload_apk_nimbledroid.py
@@ -9,15 +9,18 @@ def main():
         description='Uploads APK to nimbledroid'
     )
 
-    parser.add_argument('apk', type=argparse.FileType('r'), required=True)
+    parser.add_argument('apk', type=argparse.FileType('r'))
 
     apk = parser.parse_args().apk
     api_key = os.environ['API_KEY']
 
+    print('SKIP: Uploads {} to nimbledroid'.format(apk))
+    return
+
     response = requests.post(
         'https://nimbledroid.com/api/v2/apks',
         auth=(api_key, ''),
-        headers={'Accept', '*/*'},
+        headers={'Accept': '*/*'},
         files={'apk': apk},
         data={'auto_scenarios': 'false'}
     )
