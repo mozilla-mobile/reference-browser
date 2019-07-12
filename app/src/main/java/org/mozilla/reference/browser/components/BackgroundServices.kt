bdb96f4f5fd94ad0948e123e2261687c85da1f27
Jonathan Almeida <jalmeida@mozilla.com>
2019-07-12 14:17:43 -0400

    Update FxAAccountManager to new API

--- app/src/main/java/org/mozilla/reference/browser/components/BackgroundServices.kt
+++ app/src/main/java/org/mozilla/reference/browser/components/BackgroundServices.kt
@@ -5,16 +5,18 @@
 package org.mozilla.reference.browser.components
 
 import android.content.Context
+import android.os.Build
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import mozilla.components.browser.storage.sync.PlacesHistoryStorage
+import mozilla.components.concept.sync.DeviceCapability
 import mozilla.components.concept.sync.DeviceType
-import mozilla.components.service.fxa.Config
-import mozilla.components.feature.sync.BackgroundSyncManager
-import mozilla.components.feature.sync.GlobalSyncableStoreProvider
-import mozilla.components.service.fxa.manager.DeviceTuple
+import mozilla.components.service.fxa.DeviceConfig
+import mozilla.components.service.fxa.ServerConfig
+import mozilla.components.service.fxa.SyncConfig
 import mozilla.components.service.fxa.manager.FxaAccountManager
+import mozilla.components.service.fxa.sync.GlobalSyncableStoreProvider
 
 /**
  * Component group for background services. These are components that need to be accessed from
@@ -29,28 +31,33 @@ class BackgroundServices(
         const val REDIRECT_URL = "https://accounts.firefox.com/oauth/success/$CLIENT_ID"
     }
 
-    // This is slightly messy - here we need to know the union of all "scopes"
-    // needed by components which rely on FxA integration. If this list
-    // grows too far we probably want to find a way to determine the set
-    // at runtime.
-    private val scopes: Array<String> = arrayOf("profile", "https://identity.mozilla.com/apps/oldsync")
-    private val config = Config.release(CLIENT_ID, REDIRECT_URL)
-
     init {
         // Make the "history" store accessible to workers spawned by the sync manager.
         GlobalSyncableStoreProvider.configureStore("history" to placesHistoryStorage)
     }
 
-    val syncManager = BackgroundSyncManager("https://identity.mozilla.com/apps/oldsync").also {
-        it.addStore("history")
-    }
+    private val serverConfig = ServerConfig.release(CLIENT_ID, REDIRECT_URL)
+    private val deviceConfig = DeviceConfig(
+        name = "Reference Browser on" + Build.MANUFACTURER + " " + Build.MODEL,
+        type = DeviceType.MOBILE,
+        capabilities = setOf(DeviceCapability.SEND_TAB)
+    )
+    private val syncConfig = SyncConfig(
+        syncableStores = setOf("history"),
+        syncPeriodInMinutes = 240L
+    ) // four hours
 
     val accountManager = FxaAccountManager(
         context,
-        config,
-        scopes,
-        DeviceTuple("Reference Browser", DeviceType.MOBILE, emptyList()),
-        syncManager
+        serverConfig,
+        deviceConfig,
+        syncConfig,
+        // We don't need to specify this explicitly, but `syncConfig` may be disabled due to an 'experiments'
+        // flag. In that case, sync scope necessary for syncing won't be acquired during authentication
+        // unless we explicitly specify it below.
+        // This is a good example of an information leak at the API level.
+        // See https://github.com/mozilla-mobile/android-components/issues/3732
+        setOf("https://identity.mozilla.com/apps/oldsync")
     ).also {
         CoroutineScope(Dispatchers.Main).launch { it.initAsync().await() }
     }
