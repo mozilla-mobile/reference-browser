52216002983c4ae4216935d0f09a4712a221b689
Tiger Oakes <toakes@mozilla.com>
2019-08-01 15:50:48 -0400

    Add PWA support to reference browser

--- app/src/main/java/org/mozilla/reference/browser/ExternalAppBrowserActivity.kt
+++ app/src/main/java/org/mozilla/reference/browser/ExternalAppBrowserActivity.kt
@@ -4,7 +4,10 @@
 
 package org.mozilla.reference.browser
 
+import android.net.Uri
 import androidx.fragment.app.Fragment
+import mozilla.components.concept.engine.manifest.WebAppManifest
+import mozilla.components.feature.pwa.ext.getWebAppManifest
 import org.mozilla.reference.browser.browser.ExternalAppBrowserFragment
 
 /**
@@ -14,7 +17,21 @@ import org.mozilla.reference.browser.browser.ExternalAppBrowserFragment
 class ExternalAppBrowserActivity : BrowserActivity() {
     override fun createBrowserFragment(sessionId: String?): Fragment =
         if (sessionId != null) {
-            ExternalAppBrowserFragment.create(sessionId)
+            val manifest = intent.getWebAppManifest()
+            val scope = when (manifest?.display) {
+                WebAppManifest.DisplayMode.FULLSCREEN,
+                WebAppManifest.DisplayMode.STANDALONE -> Uri.parse(manifest.scope ?: manifest.startUrl)
+
+                WebAppManifest.DisplayMode.MINIMAL_UI,
+                WebAppManifest.DisplayMode.BROWSER -> null
+                else -> null
+            }
+
+            ExternalAppBrowserFragment.create(
+                sessionId,
+                manifest,
+                listOfNotNull(scope)
+            )
         } else {
             // Fall back to browser fragment
             super.createBrowserFragment(sessionId)
