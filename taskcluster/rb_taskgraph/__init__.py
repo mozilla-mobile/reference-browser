7e5b528750f754eb12b9769f1043bd34cadec006
Tom Prince <mozilla@hocat.ca>
2019-08-16 13:48:23 -0600

    Add routes to signing tasks.

--- taskcluster/rb_taskgraph/__init__.py
+++ taskcluster/rb_taskgraph/__init__.py
@@ -12,7 +12,7 @@ def register(graph_config):
     Import all modules that are siblings of this one, triggering decorators in
     the process.
     """
-    _import_modules(["job", "worker_types"])
+    _import_modules(["job", "worker_types", "routes"])
 
 
 def _import_modules(modules):
