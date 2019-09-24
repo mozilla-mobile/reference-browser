4560a83cb559d3d4d06383205a8bb76a44336704
Dennis Schubert <mail@dennis-schubert.de>
2019-09-24 16:46:50 +0200

    Add the WebCompat feature

--- app/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
+++ app/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
@@ -11,6 +11,7 @@ import mozilla.components.browser.engine.gecko.glean.GeckoAdapter
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.fetch.Client
+import mozilla.components.feature.webcompat.WebCompatFeature
 import mozilla.components.lib.crash.handler.CrashHandlerService
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings
@@ -40,7 +41,10 @@ object EngineProvider {
 
     fun createEngine(context: Context, defaultSettings: DefaultSettings): Engine {
         val runtime = getOrCreateRuntime(context)
-        return GeckoEngine(context, defaultSettings, runtime)
+
+        return GeckoEngine(context, defaultSettings, runtime).also {
+            WebCompatFeature.install(it)
+        }
     }
 
     fun createClient(context: Context): Client {
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -73,6 +73,7 @@ object Deps {
     const val mozilla_feature_pwa = "org.mozilla.components:feature-pwa:${Versions.mozilla_android_components}"
     const val mozilla_feature_qr = "org.mozilla.components:feature-qr:${Versions.mozilla_android_components}"
     const val mozilla_feature_readerview = "org.mozilla.components:feature-readerview:${Versions.mozilla_android_components}"
+    const val mozilla_feature_webcompat= "org.mozilla.components:feature-webcompat:${Versions.mozilla_android_components}"
 
     const val mozilla_ui_autocomplete = "org.mozilla.components:ui-autocomplete:${Versions.mozilla_android_components}"
     const val mozilla_ui_colors = "org.mozilla.components:ui-colors:${Versions.mozilla_android_components}"
