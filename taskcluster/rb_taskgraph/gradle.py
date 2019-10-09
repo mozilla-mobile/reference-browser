1605f90f9aedd8d312d845cd760460739c17c688
Johan Lorenzo <jlorenzo@mozilla.com>
2019-10-09 14:50:49 +0200

    taskgraph: Speed up gradle calls

--- taskcluster/rb_taskgraph/gradle.py
+++ taskcluster/rb_taskgraph/gradle.py
@@ -10,29 +10,46 @@ import subprocess
 from taskgraph.util.memoize import memoize
 
 
-@memoize
-def get_build_variant(variant):
-    return _run_gradle_process(
-        ["printVariant", "-PvariantBuildType={}".format(variant)], prefix="variant: "
-    )
+def get_build_variant(build_type):
+    all_variants = _fetch_all_variants()
+    matching_variants = [
+        variant for variant in all_variants
+        if variant["build_type"] == build_type
+    ]
+    number_of_matching_variants = len(matching_variants)
+    if number_of_matching_variants == 0:
+        raise ValueError('No variant found for build type "{}"'.format(build_type))
+    elif number_of_matching_variants > 1:
+        raise ValueError('Too many variants found for build type "{}": {}'.format(
+            build_type, matching_variants
+        ))
+
+    return matching_variants.pop()
 
 
 @memoize
-def get_geckoview_version():
-    print("Fetching geckoview version from gradle")
-    geckoview_version = _run_gradle_process(
-        ["printGeckoviewVersion"], prefix="geckoviewVersion: "
-    )
-    print('Got geckoview version: "{}"'.format(geckoview_version))
-    return geckoview_version
+def _fetch_all_variants():
+    output = _run_gradle_process('printVariants')
+    content = _extract_content_from_command_output(output, prefix='variants: ')
+    return json.loads(content)
 
 
-def _run_gradle_process(gradle_command, prefix):
-    output = subprocess.check_output(["./gradlew", "--quiet"] + gradle_command)
-    output = _extract_content_from_command_output(output, prefix)
+def _run_gradle_process(gradle_command, **kwargs):
+    gradle_properties = [
+        '-P{property_name}={value}'.format(property_name=property_name, value=value)
+        for property_name, value in kwargs.iteritems()
+    ]
+
+    process = subprocess.Popen(["./gradlew", "--no-daemon", "--quiet", gradle_command] + gradle_properties, stdout=subprocess.PIPE)
+    output, err = process.communicate()
+    exit_code = process.wait()
+
+    if exit_code is not 0:
+        raise RuntimeError("Gradle command returned error: {}".format(exit_code))
+
     return output
 
 
 def _extract_content_from_command_output(output, prefix):
-    prefixed_line = [line for line in output.split("\n") if line.startswith(prefix)][0]
-    return json.loads(prefixed_line[len(prefix) :])
+    variants_line = [line for line in output.split('\n') if line.startswith(prefix)][0]
+    return variants_line.split(' ', 1)[1]
