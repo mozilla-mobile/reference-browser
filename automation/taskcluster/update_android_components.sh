93cc32450b94b143dc2ceb1aa65a9d2976e3983d
Johan Lorenzo <jlorenzo@mozilla.com>
2020-03-06 15:42:13 +0100

    Bug 1620190 - part 4: Fix PR submission on update_android_components.sh

--- automation/taskcluster/update_android_components.sh
+++ automation/taskcluster/update_android_components.sh
@@ -9,22 +9,22 @@ set -ex
 apt-get install -y brew
 brew install hub
 
-BRANCH="ac-update"
-USER="MickeyMoz"
-EMAIL="sebastian@mozilla.com"
-REPO="reference-browser"
+export BRANCH="ac-update"
+export GITHUB_USER="MickeyMoz"
+export EMAIL="sebastian@mozilla.com"
+export REPO="reference-browser"
 
-git config --global user.email $EMAIL
-git config --global user.name $USER
+git config --global user.email "$EMAIL"
+git config --global user.name "$GITHUB_USER"
 
 # Fetching latest version
-LATEST_VERSION=`curl https://nightly.maven.mozilla.org/maven2/org/mozilla/components/browser-engine-gecko-nightly/maven-metadata.xml | sed -ne '/latest/{s/.*<latest>\(.*\)<\/latest>.*/\1/p;q;}'`
+LATEST_VERSION=$(curl https://nightly.maven.mozilla.org/maven2/org/mozilla/components/browser-engine-gecko-nightly/maven-metadata.xml | sed -ne '/latest/{s/.*<latest>\(.*\)<\/latest>.*/\1/p;q;}')
 
 # Updating version file
 sed -i "s/VERSION = \".*\"/VERSION = \"$LATEST_VERSION\"/g" "buildSrc/src/main/java/AndroidComponents.kt"
 
 # Create a branch and commit local changes
-git checkout -b $BRANCH
+git checkout -b "$BRANCH"
 git add buildSrc/src/main/java/AndroidComponents.kt
 git commit -m \
 	"Update Android Components version to $LATEST_VERSION." \
@@ -40,17 +40,19 @@ python automation/taskcluster/helper/get-secret.py \
 # From here on we do not want to print the commands since they contain tokens
 set +x
 
-GITHUB_TOKEN=`cat .github_token`
-URL="https://$USER:$GITHUB_TOKEN@github.com/$USER/$REPO/"
+export GITHUB_TOKEN=$(cat .github_token)
 
 # Push changes to GitHub
 echo "Pushing branch to GitHub"
-git push -f --no-verify --quiet $URL $BRANCH > /dev/null 2>&1 || echo "Failed ($?)"
+URL="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/$GITHUB_USER/$REPO/"
+hub push --force --no-verify --quiet "$URL" "$BRANCH"
 
 # Open a PR if needed
-if [[ $(hub pr list -h $USER:$BRANCH) ]]; then
+if [[ $(hub pr list --head "$GITHUB_USER:$BRANCH") ]]; then
     echo "There's already an open PR."
 else
     echo "No PR found. Opening new PR."
-    hub pull-request -b master -h MickeyMoz:ac-update --no-edit -m "Update Android Components version"
+    hub pull-request --base master --head "$GITHUB_USER:$BRANCH" --no-edit -m "Update Android Components version"
 fi
+
+unset GITHUB_TOKEN
