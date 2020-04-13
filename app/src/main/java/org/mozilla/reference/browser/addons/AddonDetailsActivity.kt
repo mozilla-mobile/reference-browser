8e040c9bf5c0f80ac045fc506cc0ed8d5e47e677
Arturo Mejia <arturomejiamarmol@gmail.com>
2020-04-13 14:53:09 -0400

    No issue: Fix add-on translate() breaking change

--- app/src/main/java/org/mozilla/reference/browser/addons/AddonDetailsActivity.kt
+++ app/src/main/java/org/mozilla/reference/browser/addons/AddonDetailsActivity.kt
@@ -14,7 +14,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import mozilla.components.feature.addons.Addon
-import mozilla.components.feature.addons.ui.translate
+import mozilla.components.feature.addons.ui.translatedDescription
+import mozilla.components.feature.addons.ui.translatedName
 import org.mozilla.reference.browser.R
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -33,7 +34,7 @@ class AddonDetailsActivity : AppCompatActivity() {
 
     private fun bind(addon: Addon) {
 
-        title = addon.translatableName.translate()
+        title = addon.translatedName
 
         bindDetails(addon)
 
@@ -91,7 +92,7 @@ class AddonDetailsActivity : AppCompatActivity() {
 
     private fun bindDetails(addon: Addon) {
         val detailsView = findViewById<TextView>(R.id.details)
-        val detailsText = addon.translatableDescription.translate()
+        val detailsText = addon.translatedDescription
 
         val parsedText = detailsText.replace("\n", "<br/>")
         val text = HtmlCompat.fromHtml(parsedText, HtmlCompat.FROM_HTML_MODE_COMPACT)
