b366017d707c2faea18d8fe85779ec479baa35dd
Johan Lorenzo <jlorenzo@mozilla.com>
2020-05-18 15:35:44 +0200

    Bug 1637891 - WIP

--- taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
+++ taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
@@ -7,4 +7,6 @@ yes | "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --licenses
 # It's nice to have the build logs include the state of the world upon completion.
 "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --list
 
+./gradlew tasks
+
 tar cf - -C "$ANDROID_SDK_ROOT" . --transform 's,^\./,android-sdk-linux/,' | xz > "$UPLOAD_DIR/android-sdk-linux.tar.xz"
