d495082f3c0ae41072b78575a0f1948c4d2c6226
Johan Lorenzo <jlorenzo@mozilla.com>
2020-05-19 18:31:40 +0200

    make both calls the same

--- taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
+++ taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
@@ -15,5 +15,5 @@ popd
 # It's nice to have the build logs include the state of the world upon completion.
 "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --list
 
-tar cf - -C "$ANDROID_SDK_ROOT" . --transform 's,^\./,android-sdk-linux/,' \
+tar cf - -C "$ANDROID_SDK_ROOT" --transform 's,^\./,android-sdk-linux/,' '.' \
   | xz > "$UPLOAD_DIR/android-sdk-linux.tar.xz"
