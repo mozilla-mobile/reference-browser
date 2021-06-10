e4fee99a77f9a9f813e01d55d9bd489513afc29d
AndiAJ <andiaj@users.noreply.github.com>
2021-06-10 17:52:26 +0300

    Custom Tabs Tests

---
+++ app/src/androidTest/java/org/mozilla/reference/browser/ui/CustomTabsTest.kt
@@ -0,0 +1,108 @@
+package org.mozilla.reference.browser.ui
+
+import androidx.test.rule.ActivityTestRule
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mozilla.reference.browser.IntentReceiverActivity
+import org.mozilla.reference.browser.helpers.AndroidAssetDispatcher
+import org.mozilla.reference.browser.helpers.BrowserActivityTestRule
+import org.mozilla.reference.browser.helpers.TestAssetHelper
+import org.mozilla.reference.browser.helpers.TestHelper.createCustomTabIntent
+import org.mozilla.reference.browser.ui.robots.customTabScreen
+
+class CustomTabsTest {
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @get:Rule
+    val activityTestRule = BrowserActivityTestRule()
+
+    @get: Rule
+    val intentReceiverActivityTestRule = ActivityTestRule(
+        IntentReceiverActivity::class.java, true, false
+    )
+
+    @Before
+    fun setUp() {
+        mockWebServer = MockWebServer().apply {
+            dispatcher = AndroidAssetDispatcher()
+            start()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun openCustomTabTest() {
+        val customTabPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+
+        intentReceiverActivityTestRule.launchActivity(
+            createCustomTabIntent(
+                customTabPage.url.toString()
+            )
+        )
+
+        customTabScreen {
+            verifyCloseButton()
+            verifyTrackingProtectionIcon()
+            verifySecurityIndicator()
+            verifyPageTitle(customTabPage.title)
+            verifyPageUrl(customTabPage.url.toString())
+            verifyActionButton()
+            verifyMenuButton()
+        }
+    }
+
+    @Test
+    fun verifyCustomTabMenuItemsTest() {
+        val customTabPage = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+
+        intentReceiverActivityTestRule.launchActivity(
+            createCustomTabIntent(
+                customTabPage.url.toString()
+            )
+        )
+
+        customTabScreen {
+        }.openMainMenu {
+            verifyForwardButton()
+            verifyRefreshButton()
+            verifyStopButton()
+            verifyShareButton()
+            verifyRequestDesktopButton()
+            verifyFindInPageButton()
+            verifyOpenInBrowserButton()
+        }
+    }
+
+    @Test
+    fun customTabNavigationTest() {
+        val pageLinks = TestAssetHelper.getGenericAsset(mockWebServer, 4)
+        val genericURL = TestAssetHelper.getGenericAsset(mockWebServer, 1)
+
+        intentReceiverActivityTestRule.launchActivity(
+            createCustomTabIntent(
+                pageLinks.url.toString()
+            )
+        )
+
+        customTabScreen {
+            clickGenericLink("Link 1")
+            verifyPageTitle(genericURL.title)
+            verifyPageUrl(genericURL.url.toString())
+        }.goBack {
+            verifyPageTitle(pageLinks.title)
+            verifyPageUrl(pageLinks.url.toString())
+        }.openMainMenu {
+            clickForwardButton()
+            verifyPageTitle(genericURL.title)
+            verifyPageUrl(genericURL.url.toString())
+        }
+    }
+}
