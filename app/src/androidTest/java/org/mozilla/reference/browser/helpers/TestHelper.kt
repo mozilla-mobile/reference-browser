38fc6e49b62205d908084f734a2a695c13c5358a
AndiAJ <andiaj@users.noreply.github.com>
2021-06-10 17:52:26 +0300

    Custom Tabs Tests

--- app/src/androidTest/java/org/mozilla/reference/browser/helpers/TestHelper.kt
+++ app/src/androidTest/java/org/mozilla/reference/browser/helpers/TestHelper.kt
@@ -4,14 +4,25 @@
 
 package org.mozilla.reference.browser.helpers
 
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
 import android.os.Build
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiScrollable
 import androidx.test.uiautomator.UiSelector
+import mozilla.components.support.ktx.android.content.appName
 
 object TestHelper {
 
     val packageName = InstrumentationRegistry.getInstrumentation().targetContext.packageName
+    val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+    val appName = appContext.appName
 
     fun scrollToElementByText(text: String): UiScrollable {
         val appView = UiScrollable(UiSelector().scrollable(true))
@@ -24,5 +35,31 @@ object TestHelper {
             true -> "com.android.permissioncontroller"
             false -> "com.android.packageinstaller"
         }
+    }
+
+    fun createCustomTabIntent(
+        pageUrl: String,
+        customActionButtonDescription: String = ""
+    ): Intent {
+        val appContext = InstrumentationRegistry.getInstrumentation()
+            .targetContext
+            .applicationContext
+        val pendingIntent = PendingIntent.getActivity(appContext, 0, Intent(), 0)
+        val customTabsIntent = CustomTabsIntent.Builder()
+            .setShareState(CustomTabsIntent.SHARE_STATE_ON)
+            .setActionButton(
+                createTestBitmap(),
+                customActionButtonDescription, pendingIntent, true
+            )
+            .build()
+        customTabsIntent.intent.data = Uri.parse(pageUrl)
+        return customTabsIntent.intent
+    }
+
+    private fun createTestBitmap(): Bitmap {
+        val bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.GREEN)
+        return bitmap
     }
 }
---
+++ app/src/androidTest/java/org/mozilla/reference/browser/ui/robots/CustomTabRobot.kt
@@ -0,0 +1,120 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.reference.browser.ui.robots
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withSubstring
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiSelector
+import org.junit.Assert.assertTrue
+import org.mozilla.reference.browser.R
+import org.mozilla.reference.browser.helpers.TestAssetHelper.waitingTime
+import org.mozilla.reference.browser.helpers.TestHelper.packageName
+import org.mozilla.reference.browser.helpers.click
+
+/**
+ *  Implementation of the robot pattern for Custom tabs
+ */
+class CustomTabRobot {
+
+    fun verifyCloseButton() = assertCloseButton()
+    fun verifyTrackingProtectionIcon() = assertTrackingProtectionIcon()
+    fun verifySecurityIndicator() = assertSecurityIndicator()
+    fun verifyPageTitle(title: String) = assertCustomTabTitle(title)
+    fun verifyPageUrl(url: String) = assertCustomTabUrl(url)
+    fun verifyActionButton() = assertActionButton()
+    fun verifyMenuButton() = assertMenuButton()
+    fun verifyForwardButton() = assertForwardButton()
+    fun verifyRefreshButton() = assertRefreshButton()
+    fun verifyStopButton() = assertStopButton()
+    fun verifyShareButton() = assertShareButton()
+    fun verifyRequestDesktopButton() = assertRequestDesktopButton()
+    fun verifyFindInPageButton() = assertFindInPageButton()
+    fun verifyOpenInBrowserButton() = assertOpenInBrowserButton()
+    fun clickForwardButton() = forwardButton().click()
+
+    fun clickGenericLink(expectedText: String) {
+        mDevice.findObject(UiSelector().resourceId("$packageName:id/engineView"))
+            .waitForExists(waitingTime)
+        mDevice.findObject(UiSelector().textContains(expectedText)).waitForExists(waitingTime)
+        val link = mDevice.findObject(By.textContains(expectedText))
+        link.click()
+    }
+
+    class Transition {
+        fun openMainMenu(interact: CustomTabRobot.() -> Unit): Transition {
+            menuButton().click()
+
+            CustomTabRobot().interact()
+            return Transition()
+        }
+
+        fun goBack(interact: CustomTabRobot.() -> Unit): Transition {
+            mDevice.pressBack()
+
+            CustomTabRobot().interact()
+            return Transition()
+        }
+    }
+}
+
+fun customTabScreen(interact: CustomTabRobot.() -> Unit): CustomTabRobot.Transition {
+    mDevice.findObject(UiSelector().resourceId("$packageName:id/toolbar")).waitForExists(waitingTime)
+    CustomTabRobot().interact()
+    return CustomTabRobot.Transition()
+}
+
+private fun closeButton() = onView(withId(R.id.mozac_browser_toolbar_navigation_actions))
+private fun trackingProtectionIcon() = onView(withId(R.id.mozac_browser_toolbar_tracking_protection_indicator))
+private fun securityIndicator() = onView(withId(R.id.mozac_browser_toolbar_security_indicator))
+private fun menuButton() = onView(withId(R.id.mozac_browser_toolbar_menu))
+private fun actionButton() = onView(withContentDescription("Share link"))
+private fun forwardButton() = onView(withContentDescription("Forward"))
+private fun refreshButton() = onView(withContentDescription("Refresh"))
+private fun stopButton() = onView(withContentDescription("Stop"))
+private fun shareButton() = onView(withText("Share"))
+private fun requestDesktopButton() = onView(withSubstring("Request desktop site"))
+private fun findInPage() = onView(withText("Find in Page"))
+private fun openInBrowser() = onView(withText("Open in Browser"))
+
+private fun assertCloseButton() =
+    closeButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertTrackingProtectionIcon() =
+    trackingProtectionIcon().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertSecurityIndicator() =
+    securityIndicator().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertMenuButton() =
+    menuButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertActionButton() =
+    actionButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertCustomTabTitle(title: String) {
+    mDevice.findObject(UiSelector().resourceId("$packageName:id/mozac_browser_toolbar_title_view"))
+        .waitForExists(waitingTime)
+    assertTrue(mDevice.findObject(UiSelector().textContains(title)).waitForExists(waitingTime))
+}
+private fun assertCustomTabUrl(url: String) {
+    mDevice.findObject(UiSelector().resourceId("$packageName:id/mozac_browser_toolbar_url_view"))
+        .waitForExists(waitingTime)
+    assertTrue(mDevice.findObject(UiSelector().textContains(url)).waitForExists(waitingTime))
+}
+private fun assertForwardButton() =
+    forwardButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertRefreshButton() =
+    refreshButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertStopButton() =
+    stopButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertShareButton() =
+    shareButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertRequestDesktopButton() =
+    requestDesktopButton().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertFindInPageButton() =
+    findInPage().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+private fun assertOpenInBrowserButton() =
+    openInBrowser().check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
