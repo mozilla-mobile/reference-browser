1dd76cfba31316e8aef7a8a4716c6b7ee81796ee
AndiAJ <andiaj@users.noreply.github.com>
2021-08-18 09:29:57 +0300

    For #1650 fix failing openLinksInAppsTest test

--- app/src/androidTest/java/org/mozilla/reference/browser/ui/robots/ExternalAppsRobot.kt
+++ app/src/androidTest/java/org/mozilla/reference/browser/ui/robots/ExternalAppsRobot.kt
@@ -44,12 +44,21 @@ private fun assertAutofillServices() {
         .waitForExists(waitingTime))
 }
 
+@Suppress("SwallowedException")
 private fun assertYouTubeApp() {
-    mDevice.waitForIdle()
-    assertTrue(mDevice.findObject(UiSelector().text("Home"))
-        .waitForExists(waitingTime))
-    assertTrue(mDevice.findObject(UiSelector().text("Subscriptions"))
-        .waitForExists(waitingTime))
+    try {
+        // Check youtube's home buttons
+        mDevice.waitForIdle()
+        assertTrue(mDevice.findObject(UiSelector().text("Home"))
+            .waitForExists(waitingTime))
+        assertTrue(mDevice.findObject(UiSelector().text("Subscriptions"))
+            .waitForExists(waitingTime))
+    } catch (e: AssertionError) {
+        println("The native youtube app opens but needs to be updated")
+        // In case the app isn't up to date on the emulator an update message will be displayed
+        assertTrue(mDevice.findObject(UiSelector().text("Update for a faster, better YouTube"))
+            .waitForExists(waitingTime))
+    }
 }
 
 private fun assertFXAQrCode() {
