a02508d2048c52a4d86bcdeb3c01ae5237f81124
Jonathan Almeida <jalmeida@mozilla.com>
2022-01-25 22:57:54 -0500

    Close #1446: Remove JCenter from build files.

--- taskcluster/scripts/toolchain/android-gradle-dependencies/after.sh
+++ taskcluster/scripts/toolchain/android-gradle-dependencies/after.sh
@@ -20,7 +20,7 @@ set -v
 pushd $WORKSPACE
 mkdir -p android-gradle-dependencies /builds/worker/artifacts
 
-cp -R ${NEXUS_WORK}/storage/jcenter android-gradle-dependencies
+cp -R ${NEXUS_WORK}/storage/central android-gradle-dependencies
 cp -R ${NEXUS_WORK}/storage/google android-gradle-dependencies
 
 tar cf - android-gradle-dependencies | xz > /builds/worker/artifacts/android-gradle-dependencies.tar.xz
