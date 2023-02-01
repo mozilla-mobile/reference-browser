6e0ad91c5dd6e3b53351ec1d8e29712396d08819
Mugurell <Mugurell@users.noreply.github.com>
2023-02-01 15:13:48 +0200

    Support breaking change from AbstractMediaSessionService
    Handle changes from https://github.com/mozilla-mobile/firefox-android/pull/505.

--- app/src/main/java/org/mozilla/reference/browser/media/MediaSessionService.kt
+++ app/src/main/java/org/mozilla/reference/browser/media/MediaSessionService.kt
@@ -5,6 +5,7 @@
 package org.mozilla.reference.browser.media
 
 import mozilla.components.browser.state.store.BrowserStore
+import mozilla.components.concept.base.crash.CrashReporting
 import mozilla.components.feature.media.service.AbstractMediaSessionService
 import org.mozilla.reference.browser.ext.components
 
@@ -13,4 +14,5 @@ import org.mozilla.reference.browser.ext.components
  */
 class MediaSessionService : AbstractMediaSessionService() {
     override val store: BrowserStore by lazy { components.core.store }
+    override val crashReporter: CrashReporting by lazy { components.analytics.crashReporter }
 }
