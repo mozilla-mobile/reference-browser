d36bd6fb2e7aeb7433a0c00edc1db98a6519b2e0
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-02-24 10:19:43 -0500

    Update Gradle to version 7.6.1 and Android Gradle Plugin to 7.4.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -25,7 +25,7 @@ object Versions {
     }
 
     object Gradle {
-        const val android_plugin = "7.3.0"
+        const val android_plugin = "7.4.1"
         const val kotlin_plugin = Kotlin.compiler
     }
 
