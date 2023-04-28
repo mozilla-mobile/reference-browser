7233394356e2a2087e5f581a30276bc30517e12d
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-04-27 23:23:37 -0400

    Update AndroidX Activity to version 1.7.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@
 // Synchronized version numbers for dependencies used by (some) modules
 object Versions {
     object AndroidX {
-        const val activity_compose = "1.6.1"
+        const val activity_compose = "1.7.1"
         const val appcompat = "1.6.1"
         const val compose = "1.3.1"
         const val constraintlayout = "2.1.4"
