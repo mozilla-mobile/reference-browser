6fe82e350713f164d96be90ed2d353314026c6f0
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-05-16 09:41:17 -0400

    Update Sentry to version 6.19.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -47,7 +47,7 @@ object Versions {
     }
 
     object ThirdParty {
-        const val sentry = "6.18.1"
+        const val sentry = "6.19.0"
     }
 
     // Workaround for a Gradle parsing bug that prevents using nested objects directly in Gradle files.
