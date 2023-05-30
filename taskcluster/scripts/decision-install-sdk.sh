b6f71bd799c1d71b0b3d21f7ef2e7156e347e2e5
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-05-30 17:58:00 -0400

    Remove JDK8 from base docker image.

--- taskcluster/scripts/decision-install-sdk.sh
+++ taskcluster/scripts/decision-install-sdk.sh
@@ -4,16 +4,14 @@ set -ex
 
 export CURL='curl --location --retry 5'
 
-ANDROID_SDK_VERSION='3859397'
-ANDROID_SDK_SHA256='444e22ce8ca0f67353bda4b85175ed3731cae3ffa695ca18119cbacef1c1bea0'
+ANDROID_SDK_VERSION='9477386'
+ANDROID_SDK_SHA256='bd1aa17c7ef10066949c88dc6c9c8d536be27f992a1f3b5a584f9bd2ba5646a0'
 SDK_ZIP_LOCATION="$HOME/sdk-tools-linux.zip"
-# For the Android build system we want Java 11. However this version of sdkmanager requires Java 8.
-JAVA8PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/:$PATH"
+JAVA11PATH="/usr/lib/jvm/java-11-openjdk-amd64/bin/:$PATH"
 
-$CURL --output "$SDK_ZIP_LOCATION" "https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_VERSION}.zip"
+$CURL --output "$SDK_ZIP_LOCATION" "https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_VERSION}_latest.zip"
 echo "$ANDROID_SDK_SHA256  $SDK_ZIP_LOCATION" | sha256sum --check
 unzip -d "$ANDROID_SDK_ROOT" "$SDK_ZIP_LOCATION"
 rm "$SDK_ZIP_LOCATION"
 
-export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
-yes | PATH=$JAVA8PATH "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --licenses
+yes | PATH=$JAVA11PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --licenses --sdk_root="${ANDROID_SDK_ROOT}"
