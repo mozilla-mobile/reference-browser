50a6c77de892539e4af89991217bdf06a9cc86c9
MickeyMoz <sebastian@mozilla.com>
2023-06-20 07:06:43 +0000

    Update Android Components version to 116.0.20230619164535.

--- buildSrc/src/main/java/AndroidComponents.kt
+++ buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "116.0.20230619040338"
+    const val VERSION = "116.0.20230619164535"
 }
