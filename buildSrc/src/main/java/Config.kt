1a47c6c90b3ede616bc4cf1800a4233b9706a9f8
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-08-14 21:44:32 -0400

    Target Android 14 (SDK 34)

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -9,9 +9,9 @@ import java.util.Locale
 
 object Config {
     // Synchronized build configuration for all modules
-    const val compileSdkVersion = 33
+    const val compileSdkVersion = 34
     const val minSdkVersion = 21
-    const val targetSdkVersion = 33
+    const val targetSdkVersion = 34
 
     @JvmStatic
     fun generateDebugVersionName(): String {
