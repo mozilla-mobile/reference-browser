8ee9b08ceda50db6f85f749a0372a0ed16eb75ca
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-08-14 21:11:33 -0400

    Update detekt to version 1.23.1 and synchronize config with firefox-android

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -41,7 +41,7 @@ object Versions {
         const val androidx_orchestrator = "1.4.2"
         const val androidx_runner = "1.5.2"
         const val androidx_uiautomator = "2.2.0"
-        const val detekt = "1.23.0"
+        const val detekt = "1.23.1"
         const val jacoco = "0.8.10"
         const val ktlint = "0.50.0"
         const val mockwebserver = "4.11.0"
