1110267527eee026c7327970636c0b45d0cadf3a
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-09-06 22:25:48 -0400

    Update AndroidX Compose to version 1.5.1 and Lifecycle to version 2.6.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -10,10 +10,10 @@ object Versions {
     object AndroidX {
         const val activity_compose = "1.7.2"
         const val appcompat = "1.6.1"
-        const val compose = "1.5.0"
+        const val compose = "1.5.1"
         const val constraintlayout = "2.1.4"
         const val core = "1.12.0"
-        const val lifecycle = "2.6.1"
+        const val lifecycle = "2.6.2"
         const val preference = "1.2.1"
         const val swiperefreshlayout = "1.1.0"
         const val work = "2.8.1"
