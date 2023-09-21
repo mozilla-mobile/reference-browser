84e6b56287698cc53a6ce16abdc8c883a020a24b
Johan Lorenzo <jlorenzo@mozilla.com>
2023-09-21 11:23:05 +0200

    Bug 1854337 - part 1: Hardcode `gradlew printVariants` in `gradle.py`

--- taskcluster/rb_taskgraph/gradle.py
+++ taskcluster/rb_taskgraph/gradle.py
@@ -3,16 +3,52 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-import json
-import subprocess
-
-from taskgraph.util.memoize import memoize
+# /!\ This implementation differs from what android-components, fenix, and focus have.
+# The main reason is: a-c is much more complex and subject to changes. The 3 projects
+# being in the same repo, they all follow the same model. This means, there's a
+# mechanism to ensure the content of `gradlew printVariants` stays in sync with the
+# .buildconfig.yml files.
+#
+# Here, we're just tracking a handful build_types, so we don't need the same complexity
+# as what was introduced in https://github.com/mozilla-mobile/firefox-android/pull/340.
+#
+# If you want to update this variable, just run `gradlew printVariants` and copy-paste
+# the result.
+_ALL_VARIANTS = [
+    {
+        "apks": [
+            {"abi": "arm64-v8a", "fileName": "app-arm64-v8a-debug.apk"},
+            {"abi": "armeabi-v7a", "fileName": "app-armeabi-v7a-debug.apk"},
+            {"abi": "x86", "fileName": "app-x86-debug.apk"},
+            {"abi": "x86_64", "fileName": "app-x86_64-debug.apk"},
+        ],
+        "build_type": "debug",
+        "name": "debug",
+    }, {
+        "apks": [
+            {"abi": "arm64-v8a", "fileName": "app-arm64-v8a-raptor-unsigned.apk"},
+            {"abi": "armeabi-v7a", "fileName": "app-armeabi-v7a-raptor-unsigned.apk"},
+            {"abi": "x86", "fileName": "app-x86-raptor-unsigned.apk"},
+            {"abi": "x86_64", "fileName": "app-x86_64-raptor-unsigned.apk"},
+        ],
+        "build_type": "raptor",
+        "name": "raptor",
+    }, {
+        "apks": [
+            {"abi": "arm64-v8a", "fileName": "app-arm64-v8a-nightly-unsigned.apk"},
+            {"abi": "armeabi-v7a", "fileName": "app-armeabi-v7a-nightly-unsigned.apk"},
+            {"abi": "x86", "fileName": "app-x86-nightly-unsigned.apk"},
+            {"abi": "x86_64", "fileName": "app-x86_64-nightly-unsigned.apk"},
+        ],
+        "build_type": "nightly",
+        "name": "nightly",
+    }
+]
 
 
 def get_build_variant(build_type):
-    all_variants = _fetch_all_variants()
     matching_variants = [
-        variant for variant in all_variants
+        variant for variant in _ALL_VARIANTS
         if variant["build_type"] == build_type
     ]
     number_of_matching_variants = len(matching_variants)
@@ -24,31 +60,3 @@ def get_build_variant(build_type):
         ))
 
     return matching_variants.pop()
-
-
-@memoize
-def _fetch_all_variants():
-    output = _run_gradle_process('printVariants')
-    content = _extract_content_from_command_output(output, prefix='variants: ')
-    return json.loads(content)
-
-
-def _run_gradle_process(gradle_command, **kwargs):
-    gradle_properties = [
-        f'-P{property_name}={value}'
-        for property_name, value in kwargs.items()
-    ]
-
-    process = subprocess.Popen(["./gradlew", "--no-daemon", "--quiet", gradle_command] + gradle_properties, stdout=subprocess.PIPE, universal_newlines=True)
-    output, err = process.communicate()
-    exit_code = process.wait()
-
-    if exit_code != 0:
-        raise RuntimeError(f"Gradle command returned error: {exit_code}")
-
-    return output
-
-
-def _extract_content_from_command_output(output, prefix):
-    variants_line = [line for line in output.split('\n') if line.startswith(prefix)][0]
-    return variants_line.split(' ', 1)[1]
