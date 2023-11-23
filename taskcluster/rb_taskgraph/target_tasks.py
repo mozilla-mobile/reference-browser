61525459316594c5ca81a7e0515a100151ea890a
Julien Cristau <jcristau@mozilla.com>
2023-11-23 10:29:31 +0100

    Protect against running multiple nightly graphs off the same revision

--- taskcluster/rb_taskgraph/target_tasks.py
+++ taskcluster/rb_taskgraph/target_tasks.py
@@ -4,6 +4,18 @@
 
 
 from taskgraph.target_tasks import _target_task as target_task
+from taskgraph.util.taskcluster import find_task_id
+
+
+def index_exists(index_path, reason=""):
+    print(f"Looking for existing index {index_path} {reason}...")
+    try:
+        task_id = find_task_id(index_path)
+        print(f"Index {index_path} exists: taskId {task_id}")
+        return True
+    except KeyError:
+        print(f"Index {index_path} doesn't exist.")
+        return False
 
 
 @target_task("nightly")
@@ -12,6 +24,19 @@ def target_tasks_nightly(full_task_graph, parameters, graph_config):
 
     def filter(task, parameters):
         return task.attributes.get("nightly", False)
+
+    index_path = (
+        f"{graph_config['trust-domain']}.v2.{parameters['project']}.branch."
+        f"{parameters['head_ref']}.revision.{parameters['head_rev']}.taskgraph.decision-nightly"
+    )
+    if os.environ.get("MOZ_AUTOMATION") and retry(
+        index_exists,
+        args=(index_path,),
+        kwargs={
+            "reason": "to avoid triggering multiple nightlies off the same revision",
+        },
+    ):
+        return []
 
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
 
