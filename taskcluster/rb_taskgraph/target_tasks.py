e0cb8a4a06e98f28185b07a066229bd875bebb95
Geoff Brown <gbrown@mozilla.com>
2023-11-24 12:36:40 -0700

    add imports missed in #2584, to fix nightly-decision task

--- taskcluster/rb_taskgraph/target_tasks.py
+++ taskcluster/rb_taskgraph/target_tasks.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 
+from redo import retry
 from taskgraph.target_tasks import _target_task as target_task
 from taskgraph.util.taskcluster import find_task_id
 
