0f8132d2bb1e9dc9f9d68ab9365a5b2b87a630b1
Roger Yang <royang@mozilla.com>
2024-02-08 10:48:22 -0500

    Update Android Components version to 124.0.20240208040258.

--- app/src/main/java/org/mozilla/reference/browser/browser/ExternalAppBrowserFragment.kt
+++ app/src/main/java/org/mozilla/reference/browser/browser/ExternalAppBrowserFragment.kt
@@ -70,9 +70,7 @@ class ExternalAppBrowserFragment : BaseBrowserFragment(), UserInteractionHandler
                 requireActivity(),
                 requireComponents.core.store,
                 sessionId,
-            ) { uri ->
-                requireComponents.analytics.crashReporter.submitCaughtException(Exception("Unknown scheme error $uri"))
-            },
+            ),
             owner = this,
             view = view,
         )
